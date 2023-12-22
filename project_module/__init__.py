7ad8a46520249e87d123c26bcd6831dd5571d5dc
Roman Lytvynenko <zoltiisen@example.com>
2023-12-22 15:56:43 +0200

    project init file updated, config.py added

--- project_module/__init__.py
+++ project_module/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import project_module.views
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
+app.config.from_pyfile('config.py', silent=True)
 
